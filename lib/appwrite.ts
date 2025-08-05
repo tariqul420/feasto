6fed1e3c5b500f02c2a4a9ebc00b9088578f6387
tariqul420 <tariqul.developer@gmail.com>
2025-08-05 23:35:12 +0600

    feat: Enhance auth screen and add Appwrite integration
    - Redesign the authentication screen with a new background and logo.
    - Add custom-button and custom-input components for a consistent UI.
    - Integrate Appwrite for backend services.
    - Add type definitions for the project.

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appWriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: 'com.tariqul.feasto',
+  databaseId: '68923a59002b611211e9',
+  userCollectionId: '68923ab4001b12c3edc6',
+};
